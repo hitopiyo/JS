63475b440400a31bd359e47d053367897e24f00a
hitopiyo <nakamurahitoe1129@gmail.com>
2024-03-28 18:41:32 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 
-let lists = document.getElementsByClassName("pull-down-list")
-console.log(lists)
+const pullDownButton = document.getElementById("lists")
+console.log("リスト")
+
+window.addEventListener("load",function(){
+    const pullDownButton = document.getElementById("lists")
+})
+    pullDownButton.addEventListener('mouseover',function(){
+        console.log("戻る")
+    })
+    pullDownButton.addEventListener('mouseout',function(){
+        console.log("外れる")
+    })
+    pullDownButton.addEventListener('click',function(){
+        console.log("クリック")
+    })
